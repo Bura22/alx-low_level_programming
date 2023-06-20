7fa2692d61761b6ffb6188fb96bc0a7efb73fe58
Biruk <burasaleamlak24@gmail.com>
2023-06-20 13:13:08 -0700

    Check if it is alphabet or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *
- *
+ * _isalpha: check if the character is alphabet or not
+ * Return: 1 if the character is alphabet
+ * and 0 if it is not alphabet
  */
 
 int _isalpha(int c)
 {
-	if ((c >=65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
