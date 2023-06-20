686e55f1d2a78ec94966271f03c098ae3cdfb35f
Biruk <burasaleamlak24@gmail.com>
2023-06-20 04:12:46 -0700

    Printing _putcha in C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
-/** main - Entry point
+/** 
+ * main - Entry point
  * Description: This program prints _putchar
- * followed by new line
+ * followed by new line.
+ * Written by Biruk
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
-	printf("_putchar\n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
