0d77e8889d524bf4eeda1f3fb38b933beb443739
Biruk <burasaleamlak24@gmail.com>
2023-06-21 11:09:17 -0700

    Printing every minutes of the day of Jack Bauer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,17 +2,17 @@
 
 /**
  * _abs - computes the abolute value of an integer
- * @n: he number to be computed 
- * Return: Always n the computed number
+ * @n: the number to be computed 
+ * Return: Always n or abs the computed number
  */
 
 int _abs(int n)
 {
-        if (n < 0)
-        {
-                int abs = n * -1;
-                return (abs);
-        }
-	return (n);
+if (n < 0)
+{
+int abs = n * -1;
+return (abs);
+}
+return (n);
 
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,6 +2,7 @@
 
 /**
  * void jack_bauer - prints every minute of the day of Jack  Bauer
+ * Description: jack_bauer takes no argument
  * Return: void
  */
 
@@ -11,7 +12,7 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for(b = 0; b <= 3; b++)
+		for (b = 0; b <= 3; b++)
 		{
 			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
@@ -19,11 +20,11 @@ void jack_bauer(void)
 				{
 					for (d = 0; d <= 9; d++)
 					{
-						_putchar(a);
-						_putchar(a);
+						_putchar(a + '0');
+						_putchar(b + '0');
 						_putchar(':');
-						_putchar(c);
-						_putchar(d);
+						_putchar(c + '0');
+						_putchar(d + '0');
 						_putchar('\n');
 					}
 				}
