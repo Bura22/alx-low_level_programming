c299f09982e1ab0962d931dbcffc07c8fd5166a4
Biruk <burasaleamlak24@gmail.com>
2023-06-22 10:41:28 -0700

    Check if it i a digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if it is a digit or not
+ * @c: The number to be checked
+ * Return: Always 1 if it id digit or 0 if it is not
+ */
+
+int _isdigit(int c)
+{
+}
